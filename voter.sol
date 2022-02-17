37b7ed6814633b093836b0564a7769e2e15ac387
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-02-17 10:44:15 +0545

    erorhandling

--- voter.sol
+++ voter.sol
@@ -1,6 +1,10 @@
 pragma solidity >=0.4.9 <0.9.0;
 pragma experimental ABIEncoderV2;
-contract voter {
+contract Voter {
+    address admin;
+    constructor(address _Admin) public {
+        admin =_Admin;
+    }
 
     uint256 VoterCounter=0;
    
@@ -15,7 +19,7 @@ contract voter {
 
 
     }
-    mapping(uint256 => Voters) public voter;
+    mapping(uint256 => Voters) public voterdet;
    
     function addVoter(string memory _voterName, address _voterAddress, string memory _voterGender, uint _age, uint _citizenshipNumber) public{
        
@@ -23,7 +27,7 @@ contract voter {
        bool newVoter=true;
        for (uint i=0;i<VoterCounter;i++)
        {
-           if(voter[i].VoterAddress == _voterAddress)
+           if(voterdet[i].VoterAddress == _voterAddress)
            {
                newVoter=false;
                break;
@@ -31,7 +35,7 @@ contract voter {
        }
        require(newVoter == true ,"Candidate is already registered");
        
-       voter[VoterCounter]= Voters(_voterName,_voterAddress,_voterGender,_age,_citizenshipNumber,false);
+       voterdet[VoterCounter]= Voters(_voterName,_voterAddress,_voterGender,_age,_citizenshipNumber,false);
        VoterCounter++;
     }
 
